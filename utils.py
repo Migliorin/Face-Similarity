ee24b9df76bd4497418a6522933c68ee22a55446
Lucas Migliorin <lucasmigliorin@hotmail.com>
2023-07-16 19:12:49 -0400

    fix: Fixing function to get images and labels

--- utils.py
+++ utils.py
@@ -3,19 +3,23 @@ import os
 import pandas as pd
 
 
-def get_images_labels(path:str) -> pd.DataFrame:
+def get_images_labels(path:str, format_img='jpeg',verbose=False) -> pd.DataFrame:
     images_paths    = []
     images_labels   = []
     count           = os.listdir(path)
     total           = len(count)
     for root,folder,file in os.walk(path):
         if(len(file) > 0):
-            print(f"Classe atual: {root} -> {len(count)}/{total}")
-            aux_      = [f'{root}/{x}' for x in file if x.endswith('.jpeg')]
-            label_    = root.split('/')[-1]
-            images_paths.extend(aux_)
-            images_labels.extend([label_]*len(aux_))
-            count.remove(label_)
+            aux_      = [f'{root}/{x}' for x in file if x.endswith(f'.{format_img}')]
+            if(len(aux_) > 0):
+                if(verbose):
+                    print(f"Classe atual: {root} -> {len(count)}/{total}")
+                label_    = root.split('/')[-1]
+                images_paths.extend(aux_)
+                images_labels.extend([label_]*len(aux_))
+                count.remove(label_)
+                
+    print("Images and labels loaded")
             
     return pd.DataFrame({
     'images_paths'   : images_paths,
